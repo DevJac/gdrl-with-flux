10ecf6be5a73c62793abbbf44a0178df7e6e55bd
DevJac <dbuttonz@gmail.com>
2021-01-27 18:16:10 -0700

    Ch8 much faster

--- Ch8.jl
+++ Ch8.jl
@@ -1,8 +1,9 @@
+using Dates
 using Flux
 using Flux.Optimise: update!
 using OpenAIGym
 using Plots
-using ProgressMeter
+using Printf
 using Statistics
 
 const env = GymEnv(:CartPole, :v1)
@@ -15,7 +16,7 @@ function Q()
     network = Chain(
         Dense(length(env.state), 512, relu),
         Dense(512, 128, relu),
-        Dense(128, length(env.actions), identity))
+        Dense(128, length(env.actions)))
     Q(network)
 end
 
@@ -26,11 +27,14 @@ struct Policy <: AbstractPolicy
     q_network :: Q
 end
 
+network_action_to_env_action(a) = a-1
+env_action_to_network_action(a) = a+1
+
 function Reinforce.action(policy::Policy, r, s, A)
     if rand() < policy.ϵ
         return rand(A)
     else
-        return argmax(policy.q_network.network(s))-1
+        return argmax(policy.q_network.network(s)) |> network_action_to_env_action
     end
 end
 
@@ -44,56 +48,74 @@ end
 
 const opt = RMSProp(0.000_5)
 
-function update(q, sars, k=40)
-    all_s = convert(Array{Float32,2}, reduce(hcat, map(x -> copy(x.s), sars)))
-    all_s′ = reduce(hcat, map(x -> copy(x.s′), sars))
-    all_a = reduce(hcat, map(x -> x.a == 0 ? [1.0, 0.0] : [0.0, 1.0], sars))
-    all_r = reduce(hcat, map(x -> x.r, sars))
-    all_f = reduce(hcat, map(x -> x.f ? 0.0 : 1.0, sars))
-    prms = params(q)
-    for k in 1:k
-        target = all_r + 1.0 * (maximum(q.network(all_s′), dims=1)) .* all_f
-        grads = gradient(prms) do
-            qs = q.network(all_s)
-            predicted = sum(qs .* all_a, dims=1)
+function optimize!(q, sars, epochs=40, γ=1.0f0)
+    γ = Float32(γ)
+    s = reduce(hcat, map(x -> x.s, sars))
+    a = reduce(hcat, map(x -> x.a == 0 ? [1.0f0, 0.0f0] : [0.0f0, 1.0f0], sars))
+    r = reduce(hcat, map(x -> x.r, sars))
+    s′ = reduce(hcat, map(x -> x.s′, sars))
+    f = reduce(hcat, map(x -> x.f ? 0.0f0 : 1.0f0, sars))
+    @assert typeof(s) == Array{Float32, 2}
+    @assert typeof(a) == Array{Float32, 2}
+    @assert typeof(r) == Array{Float32, 2}
+    @assert typeof(s′) == Array{Float32, 2}
+    @assert typeof(f) == Array{Float32, 2}
+    @assert typeof(γ) == Float32
+    for _ in 1:epochs
+        target = r + γ * maximum(q.network(s′), dims=1) .* f
+        grads = gradient(params(q)) do
+            qs = q.network(s)
+            predicted = sum(qs .* a, dims=1)
             mean((predicted .- target).^2)
         end
-        update!(opt, prms, grads)
+        update!(opt, params(q), grads)
     end
 end
 
 const batch_size = 1024
+const episode_time_limit = 500
 
-function run(n_episodes)
-    sars = SARSF[]
+function run(episode_limit=20000)
+    sars = SARSF{Vector{Float32},Int8}[]
     exploit_rewards = Float32[]
     explore_rewards = Float32[]
     q = Q()
     explore_policy = Policy(0.5, q)
     exploit_policy = Policy(0.0, q)
-    progress = Progress(n_episodes)
+    time_steps = 0
+    start_time = now()
+    newline_time = time() + 60
     try
-        for episode in 1:n_episodes
+        for episode in 1:episode_limit
             T = 0
             explore_r = run_episode(env, explore_policy) do (s, a, r, s′)
+                time_steps += 1
                 T += 1
-                @assert T <= 500
-                failed = finished(env) && T < 500
-                push!(sars, SARSF(s, a, Float32(r), s′, failed))
+                @assert T <= episode_time_limit
+                failed = finished(env) && T < episode_time_limit
+                push!(sars, SARSF(Float32.(copy(s)), Int8(a), Float32(r), Float32.(copy(s′)), failed))
                 if length(sars) >= batch_size
-                    update(q, sars)
+                    optimize!(q, sars)
                     empty!(sars)
                 end
             end
             exploit_r = run_episode(env, exploit_policy) do _
-                @assert T <= 500
+                @assert T <= episode_time_limit
             end
             push!(explore_rewards, explore_r)
             push!(exploit_rewards, exploit_r)
-            next!(progress; showvalues = [(:explore_rewards, mean(last(explore_rewards, 100))),
-                                          (:exploit_rewards, mean(last(exploit_rewards, 100)))])
+            @printf("\u1b[?25l\u1b[0E%s ep %5d ts %6d, explore r %6.2f ± %6.2f, exploit r %6.2f ± %6.2f \u1b[0K\u1b[?25h",
+                    Dates.format(Time(Nanosecond(now() - start_time)), "HH:MM:SS"),
+                    episode, time_steps,
+                    mean(last(explore_rewards, 100)), std(last(explore_rewards, 100)),
+                    mean(last(exploit_rewards, 100)), std(last(exploit_rewards, 100)))
+            if time() >= newline_time
+                newline_time += 60
+                println()
+            end
             if mean(last(exploit_rewards, 100)) >= 475; break end
         end
+        @printf("\n------> Ran %5d episodes\n\n", length(explore_rewards))
     finally
         close(env)
     end
@@ -102,7 +124,7 @@ end
 
 last(xs, n) = xs[max(1, end-n+1):end]
 
-function demo_policy(policy, n_episodes=20)
+function demo_policy(policy, n_episodes=5)
     try
         for _ in 1:n_episodes
             run_episode(env, policy) do _
@@ -121,6 +143,6 @@ function graph(rewards)
 end
 
 # To run in the REPL, include this file, then:
-# p, r, e = run(5000)
+# p, r, e = run()
 # graph(r)
 # demo_policy(p)
